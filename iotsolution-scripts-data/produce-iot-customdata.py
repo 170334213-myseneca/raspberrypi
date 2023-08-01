af1166256f4db85f05e8583ee9dfb6c6eccdd048
170334213-myseneca <133048440+170334213-myseneca@users.noreply.github.com>
2023-08-01 19:08:05 -0400

    Update produce-iot-customdata.py

--- iotsolution-scripts-data/produce-iot-customdata.py
+++ iotsolution-scripts-data/produce-iot-customdata.py
@@ -191,7 +191,7 @@ while True:
   #line = line[:-2]
   jsonline = json.loads(line)
   try:
-    lat,long,ident=getlatlong(reader,jsonline['metadata']['dsn'],'dsn')
+    lat,long,ident=getlatlong(reader)
     line = line[:-2] + "," + '"lat":' + lat + ',"long":'+long + ',"identifier":"' + ident + '"}'
     if not line:
         #break
